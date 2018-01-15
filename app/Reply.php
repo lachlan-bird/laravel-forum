9f2af2fd41c3c122c044976ab9461054b0bfb940
Lachlan Bird <lachlan.bird88@gmail.com>
2018-01-15 20:44:15 +1100

    Add backend code for polymorphic favouriting

--- app/Reply.php
+++ app/Reply.php
@@ -22,4 +22,16 @@ class Reply extends Model
     {
         return $this->belongsTo(User::class, 'user_id');
     }
+
+    public function favourites()
+    {
+        return $this->morphMany(Favourite::class, 'favourited');
+    }
+
+    public function favourite()
+    {
+        $attributes = ['user_id' => auth()->id()];
+        if(!$this->favourites()->where($attributes)->exists())
+            return $this->favourites()->create($attributes);
+    }
 }
