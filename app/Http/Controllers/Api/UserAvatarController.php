b5e7cbb66952d37504e477c4663a995fc182bb4e
Lachlan Bird <lachlan.bird88@gmail.com>
2018-07-29 12:58:07 +1000

    Docblock for previous commit of the UserAvatarController

--- app/Http/Controllers/Api/UserAvatarController.php
+++ app/Http/Controllers/Api/UserAvatarController.php
@@ -5,8 +5,15 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+/**
+ * Class UserAvatarController
+ * @package App\Http\Controllers\Api
+ */
 class UserAvatarController extends Controller
 {
+    /**
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function store()
     {
         $this->validate(request(), [
